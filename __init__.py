ebcf096458c4725c79bc689981437d532c4d5985
Buga-00 <cemal.buga@hotmail.com>
2025-10-09 15:41:44 +0200

    Fixed typo

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .students import Students
+from .students_manager import StudentsManager
 from .logger import log_reminder
 from .reminder_generator import generate_reminder
 from .reminder_sender import send_reminder
-from .scheduler import scheduler
+from .scheduler import schedule_reminders
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from students import Students
-from students_manager import StudentsManager
-from logger import log_reminder
-from reminder_generator import generate_reminder
-from reminder_sender import send_reminder
-from scheduler import schedule_reminders
+from study_reminders.students import Students
+from study_reminders.students_manager import StudentsManager
+from study_reminders.logger import log_reminder
+from study_reminders.reminder_generator import generate_reminder
+from study_reminders.reminder_sender import send_reminder
+from study_reminders.scheduler import schedule_reminders
 
 def send_reminders_now(students_manager):
     """
@@ -74,7 +74,7 @@ def main():
 
    while True:
       print("Options:")
-      print("1. end reminders now (manual test)")
+      print("1. Send reminders now (manual test)")
       print("2. Start scheduled daily reminders")
       print("3. Add a new student")
       print("4. List all students")
